7fb5417c1978f1a45e38ac2f9eeec10311e68645
airinhaals <airinhaals@gmail.com>
2026-06-26 09:40:18 -0300

    Create sobrenome.py

---
+++ sobrenome.py
@@ -0,0 +1,8 @@
+sobrenome = input("digite o sobrenome do apresentador: ")
+
+if sobrenome == "pinheiro" or sobrenome == "araujo":
+    print("Bom Dia Nação")
+elif sobrenome == "bonner" or sobrenome == "vasconcelos":
+    print("Jornal Brasileiro")
+else:
+    print("Apresentador(a) desconhecido(a)")
